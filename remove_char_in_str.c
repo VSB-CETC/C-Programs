0305c35437a3231c9719648b325a96680e93ba66
SriBalaji <iam.thecloverly@pm.me>
2023-03-17 11:54:21 +0530

    Create

---
+++ remove_char_in_str.c
@@ -0,0 +1,10 @@
+void removeCharFromString(char *str, char c) {
+    int i, j;
+    for (i = 0, j = 0; str[i] != '\0'; i++) {
+        if (str[i] != c) {
+            str[j] = str[i];
+            j++;
+        }
+    }
+    str[j] = '\0';
+}
